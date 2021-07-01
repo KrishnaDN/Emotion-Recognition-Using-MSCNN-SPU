d95d0b7cf9f1d3272c24ba1748c301bd2f017474
Krishna D N <krishnadn94@gmail.com>
2021-07-01 13:01:51 +0530

    Dataset loader

--- dataset/iemocap.py
+++ dataset/iemocap.py
@@ -1,36 +1,111 @@
 import os
 import torch.nn as nn
+import pickle
+import random
+import librosa
+import numpy as np
 
 
+LABELS = {'hap':0,'ang':1, 'exc':0,'sad':2,'neu':3}
+class TrainTestValid:
+    def __init__(self, dataset_file):
+        with open(dataset_file, 'rb') as handle:
+            self.data = pickle.load(handle)
+
+    def __call__(self,):
+        filenames = list(self.data.keys())
+        rand_idx = random.sample(range(len(filenames)), int(len(filenames)*0.2))
+        train_splits, test_splits, valid_splits = list(),list(),list()
+        for i in range(0, int(len(rand_idx)/2)):
+            idx = rand_idx[i]
+            valid_splits.append(self.data[filenames[idx]])
+        for i in range(int(len(rand_idx)/2), len(rand_idx)):
+            idx = rand_idx[i]
+            test_splits.append(self.data[filenames[idx]])
+        for idx in range(len(filenames)):
+            if idx in rand_idx:
+                continue
+            else:
+                train_splits.append(self.data[filenames[idx]])
+        return train_splits, valid_splits, test_splits
 
 class IEMOCAPDataset:
-    def __init__(self, dataset_root, glove_path, max_frames, max_words):
-        self.dataset_root = dataset_root
+    def __init__(self, dataset, glove_path, max_frames=750, max_words=50):
+        self.dataset = dataset
         self.glove_path = glove_path
         self.max_frames = max_frames
         self.max_words = max_words
+        self._load_glove
         
-    def feature_extraction(self,):
-        pass
+    def _feature_extraction(self,audio_path):
+        wav, _ = librosa.load(audio_path, sr=16000)
+        mfcc = librosa.feature.mfcc(wav, sr=16000, n_mfcc=32, win_length = 400, hop_length=160)
+        delta = librosa.feature.delta(mfcc)
+        double_delta = librosa.feature.delta(delta)
+        features = np.concatenate((mfcc, delta, double_delta), axis=0)
+        if features.shape[1]>=self.max_frames:
+            return features[:,:self.max_frames]
+        else:
+            padded_feats = np.concatenate((features, np.zeros((features.shape[0], self.max_frames-features.shape[1]))),axis=1)
+            assert padded_feats.shape[1]==self.max_frames
+            return padded_feats
     
+    @property
     def _load_glove(self,):
-        pass
+        self.word2vec = {}
+        words = []
+        with open(self.glove_path, 'rb') as f:
+            for l in f:
+                line = l.decode().split()
+                word = line[0]
+                words.append(word)
+                try:
+                    vect = np.array(line[1:]).astype(np.float)
+                    self.word2vec[word] = vect
+                except:
+                    continue
     
+    def _get_glove_features(self,text):
+        import re
+        clean_string = ' '.join(list(filter(None, re.sub(r"[^\w\d'\s]+",' ',text).split(' ')))).lower()
+        glove_feats = []
+        for word in clean_string.split(' '):
+            try:
+                glove_feats.append(self.word2vec[word])
+            except:
+                continue
+        text_feats = np.asarray(glove_feats)
+        if text_feats.shape[0]>=self.max_words:
+            return text_feats[:self.max_words,:]
+        else:
+            padded_feats = np.concatenate((text_feats, np.zeros((self.max_words-text_feats.shape[0], text_feats.shape[1]))), axis=0)
+            assert padded_feats.shape[0]==self.max_words
+            return padded_feats
+            
     def __len__(self):
-        return len(self.json_links)
+        return len(self.dataset)
+    
+    def __getitem__(self,idx):
+        item = self.dataset[idx]
+        audio_path = item['audio_path']
+        text = item['text']
+        x_vector = item['x_vector']
+        audio_feats = self._feature_extraction(audio_path)
+        text_feats = self._get_glove_features(text)
+        label = LABELS[item['label']]
+        return audio_feats, text_feats, x_vector, label
+        
+        
         
     
-    def __getitem__(self, idx):
-        json_link =self.json_links[idx]
-        masked_features,original_feats,final_phn_seq,phn_seq_len = utility.load_data(json_link)
-        #lang_label=lang_id[self.audio_links[idx].split('/')[-2]]
-        sample = {'masked_feats': torch.from_numpy(np.ascontiguousarray(masked_features)), 
-                  'gt_feats': torch.from_numpy(np.ascontiguousarray(original_feats)),
-                  'phn_seq': torch.from_numpy(np.ascontiguousarray(final_phn_seq)),
-                  'labels_length': torch.from_numpy(np.ascontiguousarray(phn_seq_len))}
-        return sample
-    
-    
+
+if __name__=='__main__':
+    dataset_file = '/media/newhd/IEMOCAP_dataset/IEMOCAP_x_vector.pickle'
+    glove_path = '/media/newhd/IEMOCAP_dataset/glove.42B.300d.txt'
+    traintestvalid = TrainTestValid(dataset_file)
+    train_splits, valid_splits, test_splits = traintestvalid()
+    gen = IEMOCAPDataset(train_splits,glove_path)
+    audio_feats, text_feats, x_vector, label = gen.__getitem__(1)
     
     
     
